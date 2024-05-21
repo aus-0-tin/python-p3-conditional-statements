9c42f2f1f3eee67db1d49106a2ea7586f0431a0b
aus-0-tin <austin.waithaka@student.moringaschool.com>
2024-05-21 08:59:07 +0300

    completes lab

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -2,16 +2,49 @@
 
 def admin_login(username, password):
     # your code here
-    pass
+     if (username.lower() == "admin") and (password == "12345"):  # .lower() for case-insensitivity
+        return "Access granted"
+     else:
+        return "Access denied"
+    
 
 def hows_the_weather(temperature):
     # your code here
-    pass
+     if temperature < 40:
+        return "It's brisk out there!"
+     elif 40 <= temperature < 65:
+        return "It's a little chilly out there!"
+     elif temperature > 85:
+        return "It's too dang hot out there!"
+     else:
+        return "It's perfect out there!"
 
-def fizzbuzz(num):
+
+
+def fizzbuzz(number):
     # your code here
-    pass
+     if number % 15 == 0:    # Check divisibility by 15 first (both 3 and 5)
+        return "FizzBuzz"
+     elif number % 3 == 0:
+        return "Fizz"
+     elif number % 5 == 0:
+        return "Buzz"
+     else:
+        return number
+
 
 def calculator(operation, num1, num2):
     # your code here
-    pass
+    if operation == '+':
+        return num1 + num2 
+    elif operation == '-':
+        return num1 - num2
+    elif operation == '*':
+        return num1 * num2
+    elif operation == '/':
+        return num1 / num2
+    else:
+        print('Invalid operation!')
+        return None
+
+    
